407c745fb8d22c17aca1da26ded7863a046b0959
bikramke <148548813+bikramke@users.noreply.github.com>
2023-10-26 10:31:36 +0530

    commit5

---
+++ com.GitHubPractice2/src/main/java/GitHubPracticeExample/GitPratiece45.java
@@ -0,0 +1,10 @@
+package GitHubPracticeExample;
+
+public class GitPratiece45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ com.GitHubPractice2/src/main/java/GitHubPracticeExample/GithubTool.java
@@ -0,0 +1,29 @@
+package GitHubPracticeExample;
+
+public class GithubTool {
+
+
+		
+	
+
+				public static void main(String[] args) {
+					// TODO Auto-generated method stub
+
+				String s1="DoggyStyle";
+				String res="";
+				
+				for(int i=0;i<s1.length();i++)
+				{
+					
+					 char c = s1.charAt(i);
+				    res=c+res;
+				    
+				}
+				System.out.println(res);
+				}
+
+			}
+
+
+
+
